3778463a8239c714b91cf8508732951f10fc25b4
matanriba <matanriba@gmail.com>
2021-08-25 11:46:37 +0300

    Basic skeleton for apps

---
+++ js/apps/mail/pages/mail-app.jsx
@@ -0,0 +1,12 @@
+
+export class MailApp extends React.Component {
+    state= {}
+
+    render() {
+        return (
+            <section>
+                <h1>Mail App!</h1>
+            </section>
+        )
+    }
+}
