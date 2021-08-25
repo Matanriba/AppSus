f001497f4eff20d52ed619497425587981935bc6
Amitee Weiner <amitweiner@gmail.com>
2021-08-25 10:44:47 +0300

    add initial files

---
+++ js/RootCmp.jsx
@@ -0,0 +1,18 @@
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+
+export function App() {
+    return (
+        <Router>
+            <header>
+                header
+            </header>
+            <main>
+                main
+            </main>
+            <footer>
+                footer
+            </footer>
+        </Router>
+    )
+}
