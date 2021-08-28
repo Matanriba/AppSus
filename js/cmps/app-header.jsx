da559917d391ecbe047167a7e0bb1e9c5a0cc604
Amitee Weiner <amitweiner@gmail.com>
2021-08-28 21:54:06 +0300

    header titles

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -7,18 +7,12 @@ export function AppHeader() {
             <div className="main-menu">
                 <button className="menu-btn"></button>
                 <nav className="dropdown-content">
-                    <NavLink to="/keep" ><button className="btn keep-btn"></button></NavLink>
-                    <NavLink to="/mail/inbox" ><button className="btn mail-btn"></button></NavLink>
-                    <NavLink to="/books"><button className="btn books-btn"></button></NavLink>
-                    <NavLink to="/about" ><button className="btn about-btn"></button></NavLink>
+                    <NavLink to="/keep" ><button className="btn keep-btn" title="Keep"></button></NavLink>
+                    <NavLink to="/mail/inbox" ><button className="btn mail-btn" title="Mail"></button></NavLink>
+                    <NavLink to="/books"><button className="btn books-btn" title="Books"></button></NavLink>
+                    <NavLink to="/about" ><button className="btn about-btn" title="About"></button></NavLink>
                 </nav>
             </div>
-            {/* <nav>
-                <NavLink to="/keep" >Keep</NavLink>
-                <NavLink to="/mail/inbox" >Mail</NavLink>
-                <NavLink to="/books">Books</NavLink>
-                <NavLink to="/about" >About</NavLink>
-            </nav> */}
         </header>
     )
 }
